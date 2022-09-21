1e17b2e1ab6346f777c48d9dc328364930255a31
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-09-21 12:28:40 -0300

    Add the new custom user model and use it as default user model
    *Add a custom manager for create_user and create_superuser
    *Add a custom model user that use the custom manager, doesn't use username and use email as the username field
    *Add a custom forms for the new custom user model
    *Add a custom admin that use the custom forms and the custom user model
    *Add the custom user model on the settings

--- entreprenapp/entreprenapp/settings.py
+++ entreprenapp/entreprenapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Setting default user model
+AUTH_USER_MODEL = 'users.CustomUser'
