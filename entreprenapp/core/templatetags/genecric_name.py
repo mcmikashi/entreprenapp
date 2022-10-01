246c3630bef48c69e35bb162c379c5688ee5fe46
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-10-01 12:47:04 -0300

    Remove custom template tags replaced by the custom context data minxin

--- entreprenapp/core/templatetags/genecric_name.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def get_class_name(instance):
-    """Return the class name of an instance"""
-    return instance.__class__.__name__
